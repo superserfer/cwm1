20014c2e639a89d3c9aed23a0531da79ccd5df26
Linus Ackermann (IT-PTR-CEN2-YPT3) <linus.ackermann2@sbb.ch>
2024-03-29 03:13:52 +0100

    improve simulation for zone2 (up to 50% faster) and remove unused files

--- monte_carlo_zone2.R
+++ monte_carlo_zone2.R
@@ -33,53 +33,43 @@ time_diff.gamm <- fitdist(time_diff$stunden, "gamma")
 # Generate Data from Exponential Distribution
 set.seed(4321)
 amount <- 1e+07
-zone_gen <- data.frame(
-  masse = rgamma(amount, shape = mass.gamm$estimate[1], rate = mass.gamm$estimate[2]),
-  velocity = rgamma(amount, shape = velocity.gamm$estimate[1], rate = velocity.gamm$estimate[2]),
-  time_diff_stunden = rgamma(amount, shape = time_diff.gamm$estimate[1], rate = time_diff.gamm$estimate[2])
-) %>% 
-  mutate(kin_energy = masse * velocity * velocity * 0.5 / 1000,)
+# if you want to simulate more than 1e+07 you have to give more RAM to with this statement 
+# options(future.globals.maxSize = really big number) -> tested with 20 GB
+masse = rgamma(amount, shape = mass.gamm$estimate[1], rate = mass.gamm$estimate[2])
+velocity = rgamma(amount, shape = velocity.gamm$estimate[1], rate = velocity.gamm$estimate[2])
+time_diff_stunden = rgamma(amount, shape = time_diff.gamm$estimate[1], rate = time_diff.gamm$estimate[2])
+kin_energy = masse * velocity * velocity * 0.5 / 1000
 
 # Function to check if net exceeds maximum
-has_net_exceed_maxium <- function(df, index, masse, time) {
+has_net_exceed_maxium <- function(index, mass_net, time_since_emptying) {
   if (index == 1) {
     return(FALSE)
   }
-  time <- time + df[index, 3]
-  if (time > 24) {
+  time_since_emptying <- time_since_emptying + time_diff_stunden[index]
+  if (time_since_emptying > 24) {
     return(FALSE)
   }
-  masse <- masse + df[index - 1, 1]
-  if (masse >= 2000) {
+  mass_net <- mass_net + masse[index - 1]
+  if (mass_net >= 2000) {
     return(TRUE)
   }
-  return(has_net_exceed_maxium(df, index-1, masse, time))
+  return(has_net_exceed_maxium(index-1, mass_net, time_since_emptying))
 }
 
-# Parallelized loop
+# Paralleled loop
 steinschlaege <- future_lapply(1:amount, function(i) {
-  count <- 0
-  if (zone_gen[i, 4] >= 1200) {
-    count <- count + 1
-  }
-  if (has_net_exceed_maxium(zone_gen, i, 0, 0) && zone_gen[i, 4] >= 600) {
-    count <- count + 1
-  }
-  return(count)
+  return(kin_energy[i] >= 1200 || (kin_energy[i] >= 600 && has_net_exceed_maxium(i, 0, 0)))
 }) %>% 
   unlist() %>% 
   sum()
 
 # Calculate simulation time
-simulation_time <- zone_gen %>% 
-  summarise(
-    years = sum(time_diff_stunden) / 24 / 365
-  )
+simulation_time <- sum(time_diff_stunden) / 24 / 365
 
 # 10s the time an car is in the danger zone, 600the amount of cars per day -> Check for more Correct Number
 prob_auto_presenc <- 10 * 600 / (24 * 60 * 60)
 prob_net_fails <-  steinschlaege / amount
-steinschlaege_per_year <- amount / simulation_time$years
+steinschlaege_per_year <- amount / simulation_time
 prob_per_year <- steinschlaege_per_year * prob_auto_presenc * prob_net_fails
 end_time <- Sys.time()
 
@@ -87,7 +77,7 @@ end_time <- Sys.time()
 print(paste("Dauer", end_time - start_time, sep = ": "))
 print(paste("Steinschläge gesammt",amount, sep = ": "))
 print(paste("Steinschläge die durch das Netzkommen", steinschlaege, sep = ": "))
-print(paste("Simuliertezeit in Jahren", simulation_time$years, sep=": "))
+print(paste("Simuliertezeit in Jahren", simulation_time, sep=": "))
 print(paste("Chance pro Steinschlag", sprintf("%.10f", prob_net_fails * prob_auto_presenc), sep = ": "))
 print(paste("Mean Steinschläge pro Jahr", steinschlaege_per_year, sep = ": "))
 print(paste("Trefferwahrscheinlickkeit in einem Jahr",sprintf("%.10f", prob_per_year), sep = ": "))
